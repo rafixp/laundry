2cd8b6dae37345e2506a36a0eaa8d02100d8916d
RAFIXP <rafiahfafauzan231@gmail.com>
2024-09-02 14:28:43 +0700

    Commit pertama aplikasi Laundry

---
+++ laundry_id.sql
@@ -0,0 +1,366 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 02, 2024 at 02:27 PM
+-- Server version: 8.0.39-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laundry_id`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksis`
+--
+
+CREATE TABLE `detail_transaksis` (
+  `id` bigint UNSIGNED NOT NULL,
+  `id_transaksi` bigint NOT NULL,
+  `id_paket` bigint NOT NULL,
+  `qty` double NOT NULL,
+  `keterangan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `id` bigint UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_kelamin` enum('L','P') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tlp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`id`, `nama`, `alamat`, `jenis_kelamin`, `tlp`, `created_at`, `updated_at`) VALUES
+(1, 'Rahayu Hayatunajah', 'Jl. Singajaya RW 02', 'P', '085323376111', '2024-09-01 23:35:25', '2024-09-01 23:43:46'),
+(2, 'Rafi Ahfa Fauzan', 'Jl. Raya Banjar RT 022 / RW 03', 'L', '086413291218', '2024-09-01 23:43:27', '2024-09-01 23:43:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_09_02_015714_create_outlets_table', 1),
+(6, '2024_09_02_015743_create_pakets_table', 1),
+(7, '2024_09_02_015753_create_transaksis_table', 1),
+(8, '2024_09_02_015803_create_members_table', 1),
+(9, '2024_09_02_015827_create_detail_transaksis_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outlets`
+--
+
+CREATE TABLE `outlets` (
+  `id` bigint UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tlp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `outlets`
+--
+
+INSERT INTO `outlets` (`id`, `nama`, `alamat`, `tlp`, `created_at`, `updated_at`) VALUES
+(5, 'Outlet Mangunreja', 'Jl. Garut -Mangunreja', '08531231126121', '2024-09-01 21:29:33', '2024-09-01 21:34:51'),
+(6, 'Outlet Garut', 'Dusun Singajaya RT. 03 RW 01', '0831216291291', '2024-09-01 21:52:38', '2024-09-01 21:52:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pakets`
+--
+
+CREATE TABLE `pakets` (
+  `id` bigint UNSIGNED NOT NULL,
+  `id_outlet` bigint NOT NULL,
+  `jenis` enum('kiloan','selimut','bed cover','kaos','lain') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_paket` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pakets`
+--
+
+INSERT INTO `pakets` (`id`, `id_outlet`, `jenis`, `nama_paket`, `harga`, `created_at`, `updated_at`) VALUES
+(2, 5, 'selimut', 'Paket Bed Cover 1', 20000, '2024-09-01 21:42:24', '2024-09-01 21:52:02'),
+(3, 6, 'selimut', 'Paket Selimut', 30000, '2024-09-01 21:53:02', '2024-09-01 21:53:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksis`
+--
+
+CREATE TABLE `transaksis` (
+  `id` bigint UNSIGNED NOT NULL,
+  `id_outlet` bigint NOT NULL,
+  `kode_invoice` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_member` bigint NOT NULL,
+  `tgl` datetime NOT NULL,
+  `batas_waktu` datetime NOT NULL,
+  `tgl_bayar` datetime NOT NULL,
+  `biaya_tambahan` int NOT NULL,
+  `diskon` double NOT NULL,
+  `pajak` int NOT NULL,
+  `status` enum('baru','proses','selesai','diambil') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dibayar` enum('dibayar','belum dibayar') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_user` bigint NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_outlet` bigint DEFAULT NULL,
+  `role` enum('admin','kasir','owner') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `id_outlet`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Rafi Ahfa Fauzan', 'admin@laundry.id', NULL, '$2y$12$GBSOCEOaWbckX6kIjFi1cuhyx2vCwqpl517sCFqs9ruepXQAihWPS', NULL, 'admin', NULL, '2024-09-01 20:44:08', '2024-09-02 00:14:32'),
+(4, 'Nurhadi', 'nurhadi@gmail.com', NULL, '$2y$12$Q2p1Ha6zhn.PGV.7mMSQLeiMdFIJAlyU1scRrcjCH63cEBo7Q2rH6', 6, 'kasir', NULL, '2024-09-02 00:12:06', '2024-09-02 00:12:06'),
+(5, 'Owner', 'owner@gmail.com', NULL, '$2y$12$i86QTkwX9gHCObGlRLOgqubtRYa4HNuSZVauiAJnUhdg4DTm2WSlC', NULL, 'owner', NULL, '2024-09-02 00:14:11', '2024-09-02 00:14:11');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_transaksis`
+--
+ALTER TABLE `detail_transaksis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `outlets`
+--
+ALTER TABLE `outlets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pakets`
+--
+ALTER TABLE `pakets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `transaksis`
+--
+ALTER TABLE `transaksis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail_transaksis`
+--
+ALTER TABLE `detail_transaksis`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `outlets`
+--
+ALTER TABLE `outlets`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pakets`
+--
+ALTER TABLE `pakets`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksis`
+--
+ALTER TABLE `transaksis`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
